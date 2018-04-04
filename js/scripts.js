046b3bbefbdba452e1b08e8ad533b069a4cad0e6
victor njuguna <victormoore254@gmail.com>
2018-04-04 08:29:11 +0300

    modify js to indicate ping-pong on numbers divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ function pingpong(num) {
   var funNumber = [];
   for (var x = 1; x <= num; x++) {
 
-    if (x % 3 === 0) {
+    if ((x % 5 === 0) && (x % 3 === 0)) {
+      funNumber.push('ping-pong');
+
+    } else if (x % 3 === 0) {
       funNumber.push('ping');
 
     } else if (x % 5 === 0) {
       funNumber.push('pong');
 
-    } else if ((x % 5 === 0) && (x % 3 === 0)) {
-      funNumber.push('pingpong');
-
-    } else {
+    }  else {
       funNumber.push(x);
 
     }
